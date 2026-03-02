35fb2cd98535736ae4a9c57905a5075bb94ef32a
SOHAM <87080389+Soham-0047@users.noreply.github.com>
2026-03-02 12:45:34 +0530

    Update solution for Concatenation of Array

--- concatenation-of-array/concatenation-of-array.cpp
+++ concatenation-of-array/concatenation-of-array.cpp
@@ -1,18 +1,18 @@
 // Concatenation of Array
 // https://leetcode.com/problems/concatenation-of-array/
-// Solved: 2026-02-27
+// Solved: 2026-03-02
 //
 class Solution {
 public:
    vector<int> getConcatenation(vector<int>& nums) {
+    
     int n = nums.size();
-    vector<int> tmp(2 * n);
-
-    for(int i = 0; i < n; i++) {
+    vector<int>tmp(2*n);
+    for(int i=0;i<n;i++){
         tmp[i] = nums[i];
-        tmp[i + n] = nums[i];
+        tmp[n+i] = nums[i];
     }
-// added
+
     return tmp;
 }
 };
