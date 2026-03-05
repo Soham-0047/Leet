692189128c2b4f3239d51ddb8e260f0b5ac575f4
SOHAM <87080389+Soham-0047@users.noreply.github.com>
2026-03-05 20:26:53 +0530

    Update solution for How Many Numbers Are Smaller Than the Current Number

--- how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -6,18 +6,38 @@ class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
         int n = nums.size();
-        vector<int>tmp(n);
-        for(int i=0;i<n;i++){
-            int count = 0;
-            for(int j=0;j<n;j++){
-                if(nums[j] < nums[i]){
-                    count++;
-                }
-            }
+        // vector<int>tmp(n);
+        // for(int i=0;i<n;i++){
+        //     int count = 0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[j] < nums[i]){
+        //             count++;
+        //         }
+        //     }
 
-            tmp[i] = count;
+        //     tmp[i] = count;
+        // }
+
+        // return tmp;
+
+        // first create a sorted array
+        vector<int>sortArray = nums;
+        sort(sortArray.begin(), sortArray.end());
+        // 1,2,2,3,8
+        unordered_map<int,int>mp;
+
+        for(int i=0;i<n;i++){
+            if(mp.find(sortArray[i])  == mp.end()){
+                mp[sortArray[i]] = i;
+            }
         }
 
-        return tmp;
+        vector<int>ans;
+
+        for(int num: nums){
+            ans.push_back(mp[num]);
+        }
+
+        return ans;
     }
 };
