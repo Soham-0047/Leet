90f2abc6fa38e75478d210c94e2551e405ae58cf
SOHAM <87080389+Soham-0047@users.noreply.github.com>
2026-03-06 10:12:27 +0530

    Update solution for How Many Numbers Are Smaller Than the Current Number

--- how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,11 +1,11 @@
 // How Many Numbers Are Smaller Than the Current Number
 // https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
-// Solved: 2026-03-05
+// Solved: 2026-03-06
 //
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        int n = nums.size();
+        // int n = nums.size();
         // vector<int>tmp(n);
         // for(int i=0;i<n;i++){
         //     int count = 0;
@@ -21,23 +21,42 @@ public:
 
         // first create a sorted array
         // With T.C = O(nlogn)
-        vector<int>sortArray = nums;
-        sort(sortArray.begin(), sortArray.end());
 
-        unordered_map<int,int>mp;
+        // vector<int>sortArray = nums;
+        // sort(sortArray.begin(), sortArray.end());
 
-        for(int i=0;i<n;i++){
-            if(mp.find(sortArray[i])  == mp.end()){
-                mp[sortArray[i]] = i;
-            }
+        // unordered_map<int,int>mp;
+
+        // for(int i=0;i<n;i++){
+        //     if(mp.find(sortArray[i])  == mp.end()){
+        //         mp[sortArray[i]] = i;
+        //     }
+        // }
+
+        // vector<int>ans;
+
+        // for(int num: nums){
+        //     ans.push_back(mp[num]);
+        // }
+
+        // return ans;
+        
+        vector<int>ct(101,0);
+
+        for(int n:nums){
+            ct[n]++;
+        }
+        for(int i=1;i<101;i++){
+            ct[i] += ct[i-1];
         }
 
-        vector<int>ans;
+        vector<int>bu;
 
-        for(int num: nums){
-            ans.push_back(mp[num]);
+        for(int n:nums){
+            if(n==0) bu.push_back(0);
+            else bu.push_back(ct[n-1]);
         }
 
-        return ans;
+        return bu;
     }
 };
