4438ba17d4e5396e2a7f9622880efa07e158774b
SOHAM <87080389+Soham-0047@users.noreply.github.com>
2026-03-17 22:49:40 +0530

    Add solution for Exclusive Time of Functions

---
+++ exclusive-time-of-functions/exclusive-time-of-functions.cpp
@@ -0,0 +1,37 @@
+// Exclusive Time of Functions
+// https://leetcode.com/problems/exclusive-time-of-functions/
+// Solved: 2026-03-17
+//
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+            vector<int> res(n, 0);
+    stack<int> st;
+    int prev = 0;
+    for (string s : logs)
+    {
+        int a = s.find(":");
+        int b = s.find(":", a + 1);
+        int id = stoi(s.substr(0, a));
+        string msg = s.substr(a + 1, b - a - 1);
+        int time = stoi(s.substr(b + 1));
+
+        if (msg == "start")
+        {
+            if (!st.empty())
+                res[st.top()] += time - prev;
+
+            st.push(id);
+            prev = time;
+        }
+        else
+        {
+            res[st.top()] += time-prev+1;
+            st.pop();
+            prev = time+1;
+        }
+    }
+
+    return res;
+    }
+};
