cdc9bd2c37ee42130886b54c6ab813d0739e3756
SOHAM <87080389+Soham-0047@users.noreply.github.com>
2026-03-31 23:50:51 +0530

    Add solution for Subsets

---
+++ subsets/subsets.cpp
@@ -0,0 +1,24 @@
+// Subsets | Medium
+// https://leetcode.com/problems/subsets/
+// Solved: 2026-03-31
+//
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> result;
+
+        for(int i=0; i < (1 << n);i++){
+            vector<int>subset;
+            for(int j=0;j<n;j++){
+                if(i & (1 << j)){
+                subset.push_back(nums[j]);
+            }
+            }
+
+             result.push_back(subset);
+        }
+
+        return result;
+    }
+};
