0cfad2875b373df8a995309af990ac4b22cc0318
SOHAM <87080389+Soham-0047@users.noreply.github.com>
2026-05-10 23:56:30 +0530

    Add solution for Maximum Number of Jumps to Reach the Last Index

---
+++ maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,23 @@
+// Maximum Number of Jumps to Reach the Last Index | Medium
+// https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/
+// Solved: 2026-05-10
+//
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        int  n = nums.size();
+        vector<int>dp(n,INT_MIN);
+        dp[0] = 0;
+        for(int i=1;i<n;i++){
+
+            for(int j=0;j<i;j++){
+
+                if(dp[j] != INT_MIN && abs(nums[i] - nums[j]) <= target){
+                    dp[i] = max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        
+        return (dp[n-1] == INT_MIN) ? -1 : dp[n-1];
+    }
+};
