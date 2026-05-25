06bab87d705b82bf4598a411a25535526db7f1fb
SOHAM <87080389+Soham-0047@users.noreply.github.com>
2026-05-25 12:05:06 +0530

    Update solution for Concatenation of Array

--- concatenation-of-array/concatenation-of-array.cpp
+++ concatenation-of-array/concatenation-of-array.cpp
@@ -1,20 +1,22 @@
-// Concatenation of Array
+// Concatenation of Array | Easy
 // https://leetcode.com/problems/concatenation-of-array/
-// Solved: 2026-03-02
+// Solved: 2026-05-25
 //
 class Solution {
 public:
-   vector<int> getConcatenation(vector<int>& nums) {
-    
-    int n = nums.size();
-    vector<int>tmp(2*n);
-    for(int i=0;i<n;i++){
-        tmp[i] = nums[i];
-        tmp[n+i] = nums[i];
-    }
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int>tmp(2*n);
+        for(int i=0;i<n;i++){
+            tmp[i] =  nums[i];
+            tmp[i+n] = nums[i];
+            // just for clean output we can do this operation one line
+            tmp[i] = tmp[i+n] = nums[i];
+        }
 
-    return tmp;
-}
+        return tmp; //O(n) and space complexity also O(n)
+    }
 };
 // 0 1 2    3 4 5
-// 
+//
