2b07eb8d38b24be9597a91eeb256966e1189969e
SOHAM <87080389+Soham-0047@users.noreply.github.com>
2026-06-17 09:51:47 +0530

    Update solution for Valid Anagram

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -4,14 +4,32 @@
 //
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
+    // bool isAnagram(string s, string t) {
+    //     if(s.length() != t.length()) return false;
+
+    //     sort(s.begin(),s.end());
+    //     sort(t.begin(), t.end());
+
+    //     if(s == t) return true;
+
+    //     return false;
+    // }
+     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
 
-        sort(s.begin(),s.end());
-        sort(t.begin(), t.end());
+        vector<int>frq(26,0);
 
-        if(s == t) return true;
+        for(auto i : s){
+            frq[i - 'a']++;
+        }
+        for(auto i : t){
+            frq[i - 'a']--;
+        }
 
-        return false;
+        for(auto mp: frq){
+            if(mp != 0) return false;
+        }
+
+        return true;
     }
 };
