81ec1e106ad5def98c115bfb37ac6736629cc849
SOHAM <87080389+Soham-0047@users.noreply.github.com>
2026-06-30 10:23:03 +0530

    Update solution for Majority Element

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -4,18 +4,31 @@
 //
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        int n = nums.size();
-        if(n<=0) return 0;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
+    int majorityElement(vector<int>& v) {
+        // unordered_map<int,int> mp;
+        // int n = nums.size();
+        // if(n<=0) return 0;
+        // for(int i=0;i<n;i++){
+        //     mp[nums[i]]++;
+        // }
+        // for(const auto &i : mp){
+        //     if(i.second > n/2){
+        //         return i.first;
+        //     }
+        // }
+        // return 0;
+        int n=  v.size();
+
+    int element = 0;
+    int ref = 0;
+
+    for(int i=0;i<n;i++){
+        if(ref == 0){
+            element = v[i];
         }
-        for(const auto &i : mp){
-            if(i.second > n/2){
-                return i.first;
-            }
-        }
-        return 0;
+
+        ref += (element == v[i]) ? 1 : -1;
+    }
+    return element;
     }
 };
